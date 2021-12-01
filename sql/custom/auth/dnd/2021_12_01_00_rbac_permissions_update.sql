20823ba8741807e4d801384b32295b3ebe25e6ff
Fl0ctioncers <Fl0ctioncers@outlook.com>
2021-12-01 22:36:09 +0100

    DB fixes

---
+++ sql/custom/auth/dnd/2021_12_01_00_rbac_permissions_update.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(882, 'dnd');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(883, 'dnd roll');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(884, 'dnd roll stat');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(885, 'dnd roll melee');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(886, 'dnd roll ranged');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(887, 'dnd roll spell');
+INSERT IGNORE INTO rbac_permissions(id, name) VALUES(888, 'dnd roll dice');
--- sql/custom/world/dnd/2021_12_01_00_command_update.sql
+++ sql/custom/world/dnd/2021_12_01_00_command_update.sql
@@ -1,5 +1,5 @@
-INSERT INTO command(name, help) VALUES('dnd roll dice', 'Syntax: .dnd roll dice <n>d<f> <b>');
-INSERT INTO command(name, help) VALUES('dnd roll stat', 'Syntax: .dnd roll stat <stat>');
-INSERT INTO command(name, help) VALUES('dnd roll melee', 'Syntax: .dnd roll melee <stat>');
-INSERT INTO command(name, help) VALUES('dnd roll ranged', 'Syntax: .dnd roll ranged <stat>');
-INSERT INTO command(name, help) VALUES('dnd roll spell', 'Syntax: .dnd roll spell <stat>');
+INSERT IGNORE INTO command(name, help) VALUES('dnd roll dice', 'Syntax: .dnd roll dice <n>d<f> <b>');
+INSERT IGNORE INTO command(name, help) VALUES('dnd roll stat', 'Syntax: .dnd roll stat <stat>');
+INSERT IGNORE INTO command(name, help) VALUES('dnd roll melee', 'Syntax: .dnd roll melee <stat>');
+INSERT IGNORE INTO command(name, help) VALUES('dnd roll ranged', 'Syntax: .dnd roll ranged <stat>');
+INSERT IGNORE INTO command(name, help) VALUES('dnd roll spell', 'Syntax: .dnd roll spell <stat>');
